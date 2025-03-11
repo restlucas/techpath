74df883612f1754b94991b9341efcf4b4312b0e0
Lucas Souza <restlucas.dev@gmail.com>
2025-03-11 15:22:32 -0300

    auth options file

---
+++ src/utils/authOptions.ts
@@ -0,0 +1,55 @@
+import { sendUserDataToBackend } from "@/services/auth.services";
+import { AuthOptions } from "next-auth";
+import GitHubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
+
+export const authOptions: AuthOptions = {
+  secret: process.env.NEXTAUTH_SECRET,
+  debug: true,
+  providers: [
+    GitHubProvider({
+      clientId: process.env.GITHUB_CLIENT_ID!,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET!,
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+  ],
+  pages: {
+    signIn: "/login",
+  },
+  jwt: {
+    maxAge: 60,
+  },
+  callbacks: {
+    async jwt({ token, user, profile, account }) {
+      if (user) {
+        const emailUsername = profile?.email?.split("@")[0] || "";
+
+        const username = (profile as { login?: string }).login || emailUsername;
+
+        try {
+          const { user: userData } = await sendUserDataToBackend(
+            user,
+            account,
+            (username as string) || "",
+          );
+
+          token.id = userData.id;
+          token.email = userData.email;
+          token.username = username;
+        } catch (error) {
+          console.error("Error sending user data to backend:", error);
+        }
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      session.user.id = token.id;
+      session.user.email = token.email;
+      session.user.username = token.username;
+      return session;
+    },
+  },
+};
