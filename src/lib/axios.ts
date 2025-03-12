b37fe8448927779ce25af85293c029c88b97208c
Lucas Souza <restlucas.dev@gmail.com>
2025-03-12 06:35:32 -0300

    small change

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { getSession } from "next-auth/react";
 
 const axiosInstance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_TECHPATH_FRONTEND_URL,
+  baseURL: process.env.NEXT_PUBLIC_TECHPATH_API_URL,
   withCredentials: true,
 });
 
